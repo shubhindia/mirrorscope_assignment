976db498a09c2b7fc1582c4f916cb6de42c2ee65
shubhindia <shubhindia123@gmail.com>
2020-08-05 22:49:01 +0530

    accept form-data instead of json to send attachment
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- emailservice/api/listing.py
+++ emailservice/api/listing.py
@@ -6,10 +6,13 @@ from api.config import readCfg
 from flask_restplus import Api, Resource, fields
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+
 import requests
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 from socket import gaierror
 import os
 from bs4 import BeautifulSoup
@@ -32,21 +35,33 @@ class KeepAlive(Resource):
 
 class SendMail(Resource):
     
+    
+    
+    def allowed_file(self, filename):
+        ALLOWED_EXTENSIONS = set(['txt','jpg','png'])
+        return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    
     def post(self):
         
         # Parameters needed to build the message
-        qid = request.json['id']
-        student_name = request.json['name']
-        student_contact = request.json['contact']
-        student_question = request.json['query']
-        student_attachment = request.json['attachment']
+        qid = request.form['id']
+        student_name = request.form['name']
+        student_contact = request.form['contact']
+        student_question = request.form['query']
+        receiver_addr = request.form['teacher_contact']
+        file = request.files['attachment']
+        if file and self.allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join("images/", filename))
         
-        print(request.json)
+        img_data = open(os.path.join("images/", filename), "rb").read()
+        image = MIMEImage(img_data, name=os.path.basename(filename))
+       
         
         sender_addr  = config.get('dev','api.u')
         sender_pass = config.get('dev','api.p')
-        receriver_addr = request.json['teacher_contact']
-        body = "Just a test mail with html"
+        
         html = """\
                 <html>
 
@@ -59,17 +74,16 @@ class SendMail(Resource):
 <td><strong>Student Name</strong></td>
 <td><strong>Student Contact</strong></td>
 <td><strong>Question</strong></td>
-<td><strong>Attachments</td>
 </tr>
 <tr>
 <td>{qid}</td>
 <td>{student_name}</td>
 <td>{student_contact}</td>
 <td>{student_question}</td>
-<td>{student_attachment}</td>
 </tr>
 </tbody>
 </table>
+
 
 </html>
 """
@@ -83,23 +97,22 @@ class SendMail(Resource):
         target.replace_with(target.replace('{student_contact}', student_contact))
         target = soup.find(text=re.compile(r'{student_question}'))
         target.replace_with(target.replace('{student_question}', student_question))
-        target = soup.find(text=re.compile(r'{student_attachment}'))
-        target.replace_with(target.replace("{student_attachment}", student_attachment))
+       
         #print(soup)
         #Setup MIME
         message = MIMEMultipart()
         message["From"] = sender_addr
-        message["To"] = receriver_addr
-        message["Subject"] = "Just a test"
-        message.attach(MIMEText(body, "plain"))
+        message["To"] = receiver_addr
+        message["Subject"] = "Student Query"
         message.attach(MIMEText(soup, "html"))
+        message.attach(image)
         
         try: 
             with smtplib.SMTP("smtp.gmail.com", 587) as server:
                 server.starttls()
                 server.login(sender_addr, sender_pass)
                 text = message.as_string()
-                server.sendmail(sender_addr, receriver_addr, text)
+                server.sendmail(sender_addr, receiver_addr, text)
                 server.quit()
                 print("Sent")
         except (gaierror, ConnectionRefusedError):
