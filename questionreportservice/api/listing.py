1ced8407adef116562d3fa5bb978f1c49769034c
shubhindia <shubhindia123@gmail.com>
2020-08-06 16:00:27 +0530

    make the deployments kubernetes ready
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- questionreportservice/api/listing.py
+++ questionreportservice/api/listing.py
@@ -6,6 +6,8 @@ from api.config import readCfg
 from flask_restplus import Api, Resource, fields
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+import os
 import requests
 
 config = read_config(['api/config/local.properties'])
@@ -23,16 +25,40 @@ class KeepAlive(Resource):
 
 class SendQuery(Resource):
     
+    def allowed_file(self, filename):
+        ALLOWED_EXTENSIONS = set(['txt','jpg','png'])
+        return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    
     def post(self):
         
        
-        attachment = request.files['attachment']
+        file = request.files['attachment']
+        if file and self.allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join("images/", filename))
+        id = request.form["id"]
         name = request.form["name"]
         contact = request.form['contact']
         question = request.form['query']
         teacher_mail = request.form['teacher_mail']
-        print(name)
-        return "ok"
+        mail = {
+            "attachment" : (filename, open(os.path.join("images/", filename), 'rb')),
+            "id" : id,
+            "name" : name,
+            "contact" : contact,
+            "query" : question,
+            "teacher_contact" : teacher_mail       
+        }
+        headers = {
+        'content-type': "multipart/form-data"
+    }
+        try:
+            
+            request.post(url="http://localhost:8081/sendemail", data=mail, headers=headers)
+        except Exception as e:
+            print("Exception")
+            print(e)
         
 api.add_resource(KeepAlive, '/keepalive')
 api.add_resource(SendQuery, '/sendquery')
